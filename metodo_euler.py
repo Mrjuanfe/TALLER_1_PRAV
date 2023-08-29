a86a40157c7742b803873dd90d5d78d73eb95069
Fabiana Alvarez <fabianaandreaalvarezgarcia@gmail.com>
2023-08-29 18:38:01 -0500

    actualizacion 1

--- metodo_euler.py
+++ metodo_euler.py
@@ -16,15 +16,34 @@ def metodo_euler(funcion_derivada, x0, y0, h, num_pasos):
 def funcion_derivada(x, y):
     return eval(function_str)
 
+##Programa principal 
+
 # Parámetros iniciales
-primer_valor= funciones_copy.pedir_numerofloat('ingrese un numero para x0: ')
-segundo_valor= funciones_copy.pedir_numerofloat('ingrese un numero para y0: ')
-paso= funciones_copy.pedir_numerofloat('ingrese el tamaño del paso (h): ')
-numero_pasos= funciones_copy.pedir_numeroint('ingrese el numero de pasos (n): ') 
+primer_valor= funciones_copy.pedir_numerofloat('Ingrese el valor inicial de x (x0): ')
+
+segundo_valor= funciones_copy.pedir_numerofloat('Ingrese el valor inicial de y y0: ')
+
+paso= funciones_copy.pedir_numerofloat('Ingrese el tamaño del paso (h): ')
+
+numero_pasos= funciones_copy.pedir_numeroint('Ingrese el numero de pasos (n): ') 
+
 function_str = input("Ingresa una función en términos de 'x' y 'y': ")
+
+##Tabulacion 
+
+print('Iteraciones\t    x\t          y\n ---------------------------------------------------------')
+
 # Resolución de la ecuación diferencial usando el método de Euler
+
 resultados =metodo_euler(funcion_derivada, primer_valor, segundo_valor, paso, numero_pasos)
+
+iteraciones = 1
+
 
 # Imprimir los resultados
 for x, y in resultados:
-    print(f"x = {x:.2f}, y = {y:.4f}")
+    print(f"{iteraciones}\t   \t x = {x:.2f},    \t y = {y:.4f}")
+    
+    iteraciones = iteraciones + 1
+    
+    
